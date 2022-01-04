7e61485bced3146af78a8a82f863cd409915d6ca
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 15:15:17 +0900

    방 목록 코드에 직접 적어서 추가(임시방편)

--- app/src/main/java/com/neppplus/jickbangcopy_20220104/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220104/MainActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.jickbangcopy_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20220104.models.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()    // 변수명의 m : 멤버변수라는 표식
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mRoomList.add(RoomData(8000, "서울지 동대문구", 5, "1번째 방입니다."))
+        mRoomList.add(RoomData(4520,"서울시 남대문구", 0, "2번째 방입니다."))
+        mRoomList.add(RoomData(100000, "서울지 동대문구", 3, "3번째 방입니다."))
+        mRoomList.add(RoomData(7500, "서울지 동작구", -1, "4번째 방입니다."))
+        mRoomList.add(RoomData(4200, "서울지 광진구", 0, "5번째 방입니다."))
+        mRoomList.add(RoomData(62100, "서울지 동대문구", -4, "6번째 방입니다."))
+        mRoomList.add(RoomData(18000, "서울지 동대문구", 7, "7번째 방입니다."))
+        mRoomList.add(RoomData(1200, "서울지 광진구", 20, "8번째 방입니다."))
+        mRoomList.add(RoomData(8800, "서울지 동대문구", 2, "9번째 방입니다."))
     }
 }
