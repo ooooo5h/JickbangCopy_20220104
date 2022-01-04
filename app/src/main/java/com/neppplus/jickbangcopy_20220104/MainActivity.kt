a6180ca5134ae73aac883a46920aecadf0de0d06
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 15:18:35 +0900

    기본 모양으로 목록이 화면에 나옴

--- app/src/main/java/com/neppplus/jickbangcopy_20220104/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220104/MainActivity.kt
@@ -2,11 +2,16 @@ package com.neppplus.jickbangcopy_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neppplus.jickbangcopy_20220104.adapters.RoomAdapter
 import com.neppplus.jickbangcopy_20220104.models.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRoomList = ArrayList<RoomData>()    // 변수명의 m : 멤버변수라는 표식
+
+    lateinit var mRoomAdapter : RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,11 @@ class MainActivity : AppCompatActivity() {
         mRoomList.add(RoomData(18000, "서울지 동대문구", 7, "7번째 방입니다."))
         mRoomList.add(RoomData(1200, "서울지 광진구", 20, "8번째 방입니다."))
         mRoomList.add(RoomData(8800, "서울지 동대문구", 2, "9번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this, mRoomList)
+        roomRecyclerView.adapter = mRoomAdapter
+
+        roomRecyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
