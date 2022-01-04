ab7d31127994cacbd57679c3ba3f748c8756f681
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 16:10:15 +0900

    방 상세보기 화면 추가   - UI 그리기 전

--- app/src/main/java/com/neppplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.jickbangcopy_20220104.R
 import com.neppplus.jickbangcopy_20220104.models.RoomData
@@ -14,7 +15,7 @@ class RoomAdapter(
     val mList : List<RoomData>
     ) : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(row : View) : RecyclerView.ViewHolder(row) {
+    inner class MyViewHolder(val row : View) : RecyclerView.ViewHolder(row) {
 
         val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
         val txtAddressAndFloor = row.findViewById<TextView>(R.id.txtAddressAndFloor)
@@ -28,6 +29,14 @@ class RoomAdapter(
             txtAddressAndFloor.text = "${data.address}, ${data.getFormattedFloor()}"
 
             txtPrice.text = data.getFormattedPrice()
+
+
+//            전체 한 줄을 표현하는 row 변수에게 클릭 이벤트 처리
+            row.setOnClickListener {
+
+//                임시 : 토스트 출력
+                Toast.makeText(mContext, "${data.description}", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
